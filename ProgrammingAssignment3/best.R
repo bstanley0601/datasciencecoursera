fbf12ca6ed2299fc0ad626857802e76381944f6f
bstanley0601 <bstanley0601@gmail.com>
2015-01-29 07:16:56 -0700

    best.R argument validations
    Added upper casing on the state argument variable to ensure comparison
    to State data frame column is accurate.

--- ProgrammingAssignment3/best.R
+++ ProgrammingAssignment3/best.R
@@ -1,11 +1,15 @@
 best <- function(state = "CO", 
                  outcomeName = "heart attack") {
-    ## Read outcome data
+    ## Read outcome data; suppress NAs
     outcomeDF <<- read.csv("./data/outcome-of-care-measures.csv", colClasses = "character")
+    outcomeDF <<- na.omit(outcomeDF)
+    ## upper case the state argument; upper case the state column; 
     ## Check that state is present in the data
+    state <- toupper(state)
+    outcomeDF$State <- toupper(outcomeDF$State)
     if (!state %in% outcomeDF$State) {
-        message("ERROR: state not found in outcome-of-care-measures.csv")
-        return(1)
+##        message("ERROR: state not found in outcome-of-care-measures.csv")
+        stop("invalid state")
     }
     ## Check that outcomeName is valid and set the column index 
     if (outcomeName == "heart attack") {
@@ -17,12 +21,13 @@ best <- function(state = "CO",
             if (outcomeName == "pneumonia") {
                 columnIdx = 23
             } else {
-                message("ERROR: Invalid outcome name.")
-                message("Valid values are 'heart attack', 'heart failure', 'pneumonia'.")
-                return(2)
+##                message("ERROR: Invalid outcome name.")
+##                message("Valid values are 'heart attack', 'heart failure', 'pneumonia'.")
+                stop("invalid outcome")
             }
         }
     }
     ## Return hospital name in that state with lowest 30-day death rate
     
+    return(bestHospital)
 }
