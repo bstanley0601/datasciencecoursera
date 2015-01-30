b2a8505446281b17b483940fb98ca808534ab2d5
bstanley0601 <bstanley0601@gmail.com>
2015-01-30 07:38:44 -0700

    PA3
    final version with results

--- ProgrammingAssignment3/best.R
+++ ProgrammingAssignment3/best.R
@@ -1,3 +1,4 @@
+library(data.table); library(plyr); library(dplyr)
 best <- function(state = NA, 
                  outcomeName = NA) {
     ## Read outcome data; suppress NAs
@@ -12,7 +13,11 @@ best <- function(state = NA,
         stop("invalid state")
     }
     ## Check that outcomeName is valid and set the column index 
-    if (outcomeName == "heart attack") {
+    if (is.na(outcomeName)) {
+        stop("invalid outcome")
+    }
+    outcomeName <- tolower(outcomeName)
+    if (outcomeName == "heart attack" ) {
         columnIdx = 11
     } else {
         if (outcomeName == "heart failure") {
@@ -30,11 +35,36 @@ best <- function(state = NA,
     ## Return hospital name in that state with lowest 30-day death rate
     
     ## Subset the data by state and outcome
-    outcomeSubset <- outcomeDF[outcomeDF$State==state,columnIdx]
+    outcomeSubset <- outcomeDF[outcomeDF$State==state,c(2,columnIdx,7)]
+    ## rename the outcome column to something useful
+    colnames(outcomeSubset)[2] <- "OutCome"
     ## remove the larger data frame
+    rm(outcomeDF)
     ## cast the outcome column to numeric and suppress the warnings about NAs
-    outcomeSubset <- suppressWarnings(as.numeric(outcomeSubset))
+    outcomeSubset$OutCome <- suppressWarnings(as.numeric(outcomeSubset$OutCome))
     ##omit NAs
     outcomeSubset <- na.omit(outcomeSubset)
- #   return(bestHospital)
+    outcomeDT <- data.table(outcomeSubset, key="OutCome")
+    outcomeBest <- head(outcomeDT[,list(best=min(OutCome)), by=Hospital.Name], 1L)
+    
+    outcomeBest$Hospital.Name
+    
 }
+
+##> best("TX","heart attack")
+##[1] "CYPRESS FAIRBANKS MEDICAL CENTER"
+
+##> best("TX","heart failure")
+##[1] "FORT DUNCAN MEDICAL CENTER"
+
+##> best("MD", "heart attack")
+##[1] "JOHNS HOPKINS HOSPITAL, THE"
+
+##> best("MD", "pneumonia")
+##[1] "GREATER BALTIMORE MEDICAL CENTER"
+
+##> best("BB", "heart attack")
+##Error in best("BB", "heart attack") : invalid state
+
+##> best("NY", "hert attack")
+##Error in best("NY", "hert attack") : invalid outcome
